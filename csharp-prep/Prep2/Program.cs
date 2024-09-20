1adff1a3edaf7db7ee2915cb902c2d349ffb63eb
cinpaezita <pae24002@byui.edu>
2024-09-20 15:13:50 -0400

    C# Prep 2—Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade percentage? ");
+        string studentAnswer = Console.ReadLine();
+        int percent = int.Parse(studentAnswer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        /// Stretch Challenge 1
+        string sign = "";
+        int lastDigit = percent % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+        /// Stretch Challenge 2
+        if (percent >= 93)
+        {
+            sign = "";
+        }
+        /// Stretch Challenge 1
+         if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations. You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Stay focused and you'll get it next time!");
+        }
+
     }
 }
