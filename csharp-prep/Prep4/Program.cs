caab83c56d574f27ffc9871a6fba217fca9b51a3
cinpaezita <pae24002@byui.edu>
2024-09-20 15:14:25 -0400

    C# Prep 4—Lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int user = -1;
+
+        while (user != 0)
+        {
+            Console.Write("Enter a number: ");
+
+            string userNumber = Console.ReadLine();
+            user = int.Parse(userNumber);
+
+            if (user != 0)
+            {
+                numbers.Add(user);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+        
     }
 }
