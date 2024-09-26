9b34220c98a23d1fb1082b175ca9d9952df3cb90
cinpaezita <pae24002@byui.edu>
2024-09-26 16:01:11 -0400

    Team Activity: Designer

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,61 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Journal theJournal = newJournal();
+        //PromptGenerator promptGenerator = new PromptGenerator();
+
+        //Menu
+        while(true)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do?: ");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                //Create a new entry//
+
+
+            }
+
+            else if (option == "2")
+            {
+                //Display all entries
+
+            }
+
+            else if (option == "3")
+            {
+                //Load the journal
+
+            }
+
+            else if (option == "4")
+            {
+                //Save the journal
+
+            }
+
+            else if (option == "5")
+            {
+                //Exit the program
+
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid option, please try again.");
+            }
+        }
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,10 @@
+public class Generator
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+}
