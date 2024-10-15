be108580d66edf74e0d58c928a23549ccee66098
cinpaezita <pae24002@byui.edu>
2024-10-15 14:42:52 -0300

    Inheritance Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assg1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assg1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assg2 = new MathAssignment("Roberto Rodriguez", "Fraction", "7.3", "8-19");
+        Console.WriteLine(assg2.GetSummary());
+        Console.WriteLine(assg2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assg3 = new WritingAssignment("Mary Waters", "European History", "The causes of World War II");
+        Console.WriteLine(assg3.GetSummary());
+        Console.WriteLine(assg3.GetWritingInformation());
     }
 }
