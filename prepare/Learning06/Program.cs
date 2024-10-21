070b7fc01d2c7a87966f4b79602d1b4ae9fc0b4e
cinpaezita <pae24002@byui.edu>
2024-10-21 11:15:35 -0300

    Polymorphism Learning Activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Green", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Purple", 7, 8);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Orange", 9);
+        shapes.Add(shape3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The area of the {color} shape is {area}.");
+        }
     }
 }
